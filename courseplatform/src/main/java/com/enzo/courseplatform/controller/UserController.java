3541dd6b45df971217925c7f12eebc9b17a3b226
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-04 20:49:31 -0300

    UserController and UserService

--- courseplatform/src/main/java/com/enzo/courseplatform/controller/UserController.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.enzo.courseplatform.controller;
 
+import com.enzo.courseplatform.dto.request.CreateUsersRequest;
+import com.enzo.courseplatform.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    public void createUser(@RequestBody @Valid CreateUsersRequest request){
+        userService.createUser(request);
+    }
+
+
 }
---
+++ courseplatform/src/main/java/com/enzo/courseplatform/service/UserService.java
@@ -0,0 +1,25 @@
+package com.enzo.courseplatform.service;
+
+import com.enzo.courseplatform.dto.request.CreateUsersRequest;
+import com.enzo.courseplatform.model.User;
+import com.enzo.courseplatform.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void createUser(CreateUsersRequest request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        userRepository.save(user);
+    }
+}
