7b9f0e7ed25842459dfdfe90404033c759c79a26
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-08 19:34:51 -0300

    UsercController methods

--- courseplatform/src/main/java/com/enzo/courseplatform/controller/CourseController.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/controller/CourseController.java
@@ -1,9 +1,15 @@
 package com.enzo.courseplatform.controller;
 
 
+import com.enzo.courseplatform.dto.request.CreateCourseRequest;
+import com.enzo.courseplatform.dto.request.UpdateCourseRequest;
+import com.enzo.courseplatform.dto.response.CourseResponseDTO;
 import com.enzo.courseplatform.service.CourseService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -15,5 +21,30 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    @GetMapping
+    public List<CourseResponseDTO> getAll(){
+        return courseService.getAll();
+    }
 
+    @GetMapping("/{id}")
+    public CourseResponseDTO getById(@PathVariable Integer id){
+        return courseService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createCourse(@RequestBody @Valid CreateCourseRequest request){
+        courseService.createCourse(request);
+    }
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCourse(@PathVariable Integer id, @RequestBody @Valid UpdateCourseRequest request){
+        courseService.updateUser(id,request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCourse(@PathVariable Integer id){
+        courseService.deleteCourse(id);
+    }
 }
