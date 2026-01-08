74c555af8b56d29bdcdd257c6e61f475064ef2d6
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-07 22:31:00 -0300

    Create course and FindByID course

--- courseplatform/src/main/java/com/enzo/courseplatform/service/CourseService.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/service/CourseService.java
@@ -1,17 +1,36 @@
 package com.enzo.courseplatform.service;
 
+import com.enzo.courseplatform.dto.request.CreateCourseRequest;
+import com.enzo.courseplatform.dto.response.CourseResponseDTO;
+import com.enzo.courseplatform.exception.ResourceNotFoundException;
+import com.enzo.courseplatform.model.Course;
 import com.enzo.courseplatform.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CourseService {
+
     private final CourseRepository courseRepository;
 
-    public CourseService(CourseRepository courseRepository){
+    public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
+    public void createCourse(CreateCourseRequest request){
+        Course course = new Course();
+        course.setTitle(request.getTitle());
+        course.setDescription(request.getDescription());
+        course.setWorkloadHours(request.getWorkloadHours());
+        courseRepository.save(course);
+    }
+
+    public CourseResponseDTO getById(Integer id){
+        Course course = courseRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Course not found"));
+        return CourseResponseDTO.fromEntity(course);
+    }
+
+    
 
 
 }
