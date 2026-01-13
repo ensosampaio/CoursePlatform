60b994c74cbba1edb18707134d7f2e73b0c67807
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-12 23:07:19 -0300

    Update SecurityConfig.java

--- courseplatform/src/main/java/com/enzo/courseplatform/config/SecurityConfig.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-        
+
 
     }
 
