455cffeb1274db0d33c9cb2c2f861fb70f91928a
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-13 18:26:38 -0300

    Update SecurityConfig.java

--- courseplatform/src/main/java/com/enzo/courseplatform/config/SecurityConfig.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.enzo.courseplatform.config;
 
+import com.enzo.courseplatform.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,20 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtService jwtService) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                ).addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
