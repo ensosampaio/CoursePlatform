6de0064dca146275708bebf4b7ebf29efef8d228
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-14 21:22:34 -0300

    Update CreateEnrollmentRequest.java

--- courseplatform/src/main/java/com/enzo/courseplatform/dto/request/CreateEnrollmentRequest.java
+++ courseplatform/src/main/java/com/enzo/courseplatform/dto/request/CreateEnrollmentRequest.java
@@ -3,7 +3,6 @@ package com.enzo.courseplatform.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateEnrollmentRequest(
-        @NotNull(message = "User id is required") Integer userId,
         @NotNull(message = "Course id is required") Integer courseId
 ){
 }
