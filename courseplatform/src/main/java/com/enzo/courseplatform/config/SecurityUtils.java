6773397bcaef602774d552fae58a8f6b4a092974
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-14 21:23:51 -0300

    Create SecurityUtils.java

---
+++ courseplatform/src/main/java/com/enzo/courseplatform/config/SecurityUtils.java
@@ -0,0 +1,12 @@
+package com.enzo.courseplatform.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+
+    public static String getAuthenticatedEmail(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+}
