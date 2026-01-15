1b189ba2071f907f1ee9cbed945ec30325ee2f02
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-14 21:56:27 -0300

    Create UserControllerTest

--- courseplatform/src/test/java/com/enzo/courseplatform/UserControllerTest.java
+++ courseplatform/src/test/java/com/enzo/courseplatform/UserControllerTest.java
@@ -1,4 +1,40 @@
 package com.enzo.courseplatform;
 
+import com.enzo.courseplatform.dto.request.CreateUserRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldCreateUser() throws Exception {
+        CreateUserRequest request = new CreateUserRequest(
+                "Enzo",
+                "enzo@test.com",
+                "123456"
+        );
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
 }
+
